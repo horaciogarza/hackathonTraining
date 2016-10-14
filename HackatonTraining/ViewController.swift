dd5e4ecc89e5ad312a20632be0e7512b27c16aad
Abraham Barcenas M <abrahamz96@hotmail.com>
2016-10-14 17:34:16 -0500

    Creacion de proycto, proyecto vacio

---
+++ HackatonTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackatonTraining
+//
+//  Created by Abraham Barcenas M on 10/14/16.
+//  Copyright © 2016 Fimeños. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
