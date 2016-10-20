d07775da0baea59ce06c42b943a7318f70d19152
Horacio Garza <hhgarza95@gmail.com>
2016-10-19 19:54:20 -0500

    sum

--- HackatonTraining/ViewController.swift
+++ HackatonTraining/ViewController.swift
@@ -12,12 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let a = doSum(5, 5)
+        
+        
+        print(a)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func doSum(_ a: Int, _ b: Int) -> Int{
+        return 10
     }
 
 
