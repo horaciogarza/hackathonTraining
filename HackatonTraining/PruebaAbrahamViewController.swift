59cea6889de1fa2db2b03939a318e19be592c219
Abraham Barcenas M <abrahamz96@hotmail.com>
2016-10-19 20:22:58 -0500

    Login con controller

---
+++ HackatonTraining/PruebaAbrahamViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PruebaAbrahamViewController.swift
+//  HackatonTraining
+//
+//  Created by Abraham Barcenas M on 10/19/16.
+//  Copyright © 2016 Fimeños. All rights reserved.
+//
+
+import UIKit
+
+class PruebaAbrahamViewController: UIViewController {
+
+    @IBOutlet var txtEmail: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        
+    }
+
+
+}
